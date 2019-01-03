d545b73faf37ce4c851e5962cc12df18bdf39e07
zhouqunhua <qunhua.zhou@audaque.com>
2019-01-03 17:53:08 +0800

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,12 +4,8 @@ const router = require('koa-router')()
 
 router.prefix('/api')
 
-const wx = require('./wx')
-
 const user = require('./user')
 const brand = require('./brand')
-
-router.use(wx.routes(), wx.allowedMethods())
 
 router.use(user.routes(), user.allowedMethods())
 router.use(brand.routes(), brand.allowedMethods())
